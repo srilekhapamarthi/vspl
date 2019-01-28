9835d20cbf51142506393c4ba5aa87f58f883cdc
srilekhapamarthi <psrilekha4648@gmail.com>
2019-01-28 16:03:48 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -2,13 +2,13 @@ var page="";
 
 function viewSection(pageIdentifier,sectionIdentifier) {
     //find whether the page is already loaded
-    pageLoad(pageIdentifier);
+    pageLoad(pageIdentifier,sectionIdentifier);
     //if page is already loaded then scroll to particular section in that page
     //if page is not loaded then do ajax request and scroll to that section
-    scrollToSec(sectionIdentifier);
+    // scrollToSec(sectionIdentifier);
 }
 
-function pageLoad(pageIdentify){
+function pageLoad(pageIdentify,sectionIdentifier){
     // finds if requested page is already loaded, if not it loads the page.
     var allPages=["vspl_company.html","terminal_details.html","berth_position.html","our_policy.html","careers.html","contact_us.html","important_links.html"];
     if(page!=pageIdentify){
@@ -16,15 +16,18 @@ function pageLoad(pageIdentify){
         xhttp.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
             document.getElementById("demo").innerHTML = this.responseText;
+             scrollToSec(sectionIdentifier);
           }
         };
         xhttp.open("GET",allPages[pageIdentify] , true);
         xhttp.send();
     }
+    else
+    scrollToSec(sectionIdentifier);
   
 }
 function scrollToSec(sectionIdentify){
     //scrolls the page to required section
     var elmnt = document.getElementById(sectionIdentify);
-    elmnt.scrollIntoView();
+        elmnt.scrollIntoView();
 }
