fb4678a4cf5bec08e38983d6a5677a5d5f21443c
mbsyaswanth <mbs.yaswanth@gmail.com>
2019-02-02 11:14:51 +0530

    adding db

--- db/curr_vessels_entry_db.js
+++ db/curr_vessels_entry_db.js
@@ -1,6 +1,14 @@
 // Steps to insert current vessels to firebase
-
 // 1) check whether the user is logged in.
 //   1-a) If user is logged in, then submit the entry form.
 //     1-a-1) collect all the input fields using DOM , form a json and submit it to firebase.
 //   1-b) If user is not logged in, then dont submit the entry form.redirect him to a login page.
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      
+    } else {
+      // User is signed out.
+      // ...
+    }
+  });
