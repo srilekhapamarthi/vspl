bb416ee3062250f4d9efea2feefd17d17221ad0e
srilekhapamarthi <psrilekha4648@gmail.com>
2019-02-04 18:14:52 +0530

    me

--- footer-animation.js
+++ footer-animation.js
@@ -12,7 +12,7 @@ resizeCanvas();
 var wave1 = createWave(context, {
   amplitude: 50,
   duration: 1,
-  fillStyle: "rgba(24,56,127,0.8)",
+  fillStyle: "rgba(18,97,160,0.8)",
   frequency: 2.5,
   width: vw,
   height: vh,
